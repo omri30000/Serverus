912904d5916bf87163a7ce2c5a4d291b512d3bf2
Omri Zaiman <omri162004@gmail.com>
2020-11-08 18:19:55 +0200

    added basic version of sniffer

--- Sniffer.py
+++ Sniffer.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+
 # This is the sniffer file, it will parse the data from the sniffs to a data for the model
 
-from scapy import *
+from scapy.all import *
+import socket
+import datetime
+import os
+#  from geoip import geolite2
+import time
+
+def main():
+    sniff(prn=parse_packet)
+
+
+def parse_packet(pkt):
+    # get the packet arrival time
+    time = datetime.datetime.now()
+    pkt.show()
+
+
+
+if __name__ == '__main__':
+    main()
