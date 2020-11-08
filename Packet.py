0f48174560c2da38a26baa2f8e75c241598d63a2
Omri Zaiman <omri162004@gmail.com>
2020-11-08 16:05:02 +0200

    added packet, time, and sniffer files

---
+++ Packet.py
@@ -0,0 +1,12 @@
+class Packet:
+    def __init__(self, all_data):
+        """
+        all_data: the whole data of the packet
+        """
+        arrival_time = 0
+        source_IP = "127.0.0.1"
+        dest_IP = "127.0.0.1"
+        protocol = "UDP"
+        data = "abcdefg"
+
+    
---
+++ Sniffer.py
@@ -0,0 +1,3 @@
+# This is the sniffer file, it will parse the data from the sniffs to a data for the model
+
+from scapy import *
