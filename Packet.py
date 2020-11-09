87a85e9401c7a9b2ca2748d89366ed362f468dfb
Omri Zaiman <omri162004@gmail.com>
2020-11-09 14:47:13 +0200

    added packet parsing to packet class

--- Packet.py
+++ Packet.py
@@ -1,14 +1,41 @@
+from scapy.all import *
+
 class Packet:
-    def __init__(self, all_data):
+    def __init__(self, time, pack):
         """
+        time: the arrival time of the packet
         all_data: the whole data of the packet
         """
-        arrival_time = 0
-        source_IP = "127.0.0.1"
-        dest_IP = "127.0.0.1"
-        protocol = "UDP"
-        data = "abcdefg"
+        self.arrival_time = time
+
+        if Ether in pack:
+            self.source_mac = pack[Ether].src
+        else:
+            raise Exception("can't create packet")
+        
+        if IP in pack:
+            self.source_IP = pack[IP].src
+            self.dest_IP = pack[IP].dst
+        else:
+            raise Exception("can't create packet")
+        
+        try:
+            self.source_port = pack[UDP].sport
+            self.dest_port = pack[UDP].dport
+            self.protocol = "UDP"
+        except:
+            try: 
+                self.source_port = pack[TCP].sport
+                self.dest_port = pack[TCP].dport
+                self.protocol = "TCP"
+            except:
+                raise Exception("can't create packet")
+            
+        try:
+            self.length = pack.len
+            self.data = pack[Raw].load
+        except:
+            raise Exception("can't create packet")
 
     def to_string(self):
         pass
-    
