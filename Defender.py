dfef1ae8ed929e4edb29e6dead544da70ec19e31
sharp30 <ofirs2003@gmail.com>
2020-11-09 15:54:32 +0200

    build defender class structure

---
+++ Defender.py
@@ -0,0 +1,16 @@
+class Defender():
+    def init():
+    
+    def defend(entity,level):
+        pass
+    def cancelAction(entity,level):
+        pass
+
+    def close_socket(enetity):
+        pass
+
+    def block(rule):
+        pass
+
+    def inform(rule):
+        pass
