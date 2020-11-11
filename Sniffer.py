acfa665725a22328e3c47476ca4270291b4fd33a
Omri Zaiman <omri162004@gmail.com>
2020-11-11 16:45:48 +0200

    added Sniffer class

--- Sniffer.py
+++ Sniffer.py
@@ -15,47 +15,51 @@ import csv
 import queue
 import threading
 
+class Sniffer:
+    def __init__(self):
+        self._packets_queue = queue.Queue()
 
-packets_queue = queue.Queue()
+
+    def start_sniffing(self):
+        threading.Thread(target=self.__handle_packets_queue, daemon=True).start()
+        sniff(prn=self.__parse_packet, count=20)
+    
+
+    def __handle_packets_queue(self):
+        with open('sniffs.csv', 'a', newline='') as csvfile:
+            fieldnames = ['source_mac', 'source_IP', 'dest_IP', 'source_port', 
+            'dest_port', 'protocol', 'length', 'data', 'arrival_time']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            while True:
+                if not self._packets_queue.empty():
+                    pack = self._packets_queue.get()
+                    
+                    writer.writerow(pack.asdict())
+                    
+                    self._packets_queue.task_done()
+
+    def __parse_packet(self, pkt):
+        # print('hello')
+        # get the packet arrival time
+
+        # todo: fix time issues and send it to Packet
+        # time = datetime.datetime.now()
+
+        try:
+            self._packets_queue.put(Packet(15, pkt))
+        except Exception as e:
+            print(e)
+
+        self._packets_queue.join()
+
 
 def main():
-    threading.Thread(target=handle_packets_queue, daemon=True).start()
-    sniff(prn=parse_packet, count=20)
-    
+    mySniffer = Sniffer()
 
-def handle_packets_queue():
-    global packets_queue
-    with open('sniffs.csv', 'a', newline='') as csvfile:
-        fieldnames = ['source_mac', 'source_IP', 'dest_IP', 'source_port', 
-        'dest_port', 'protocol', 'length', 'data', 'arrival_time']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        while True:
-            if not packets_queue.empty():
-                pack = packets_queue.get()
-                
-                writer.writerow(pack.asdict())
-                
-                packets_queue.task_done()
-
-def parse_packet(pkt):
-    global packets_queue
-
-    # print('hello')
-    # get the packet arrival time
-
-    # todo: fix time issues and send it to Packet
-    # time = datetime.datetime.now()
-
-    try:
-        packets_queue.put(Packet(15, pkt))
-    except Exception as e:
-        print(e)
-
-    packets_queue.join()
-    
+    mySniffer.start_sniffing()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
