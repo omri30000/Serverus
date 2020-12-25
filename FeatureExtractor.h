7e392d80ed842e2e5fd5ec34f3db478e7d5295d6
sharp30 <ofirs2003@gmail.com>
2020-12-25 14:48:54 +0200

    add FeatureExtractor class declaration

---
+++ FeatureExtractor.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+//TODO: add local includes
+using std::vector;
+
+class FeatureExtractor:
+{
+protected:
+    vector<float> _lambdas;
+    
+    IncStatsData _channelJitter;
+    IncStatsData _channelSize;
+    IncStatsData _socketSize;
+
+
+public:
+    //option for deafult values
+    FeatureExtractor();
+    FeatureExtractor(vector<float> lambdas);
+
+    vector<float> extractNewFeaturesVector(Packet pack);
+}
