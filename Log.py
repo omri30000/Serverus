aa48d776008d44ee61ab3463b4eb58ba852eff6a
Omri Zaiman <omri162004@gmail.com>
2020-12-26 16:42:36 +0200

    added Log class declaration

---
+++ Log.py
@@ -0,0 +1,11 @@
+import Entity
+
+class Log():
+    
+    
+    def __init__(self, log_path):
+        self.file_path = log_path
+
+    def add_record(self, entity, level):
+        #TODO: fill this function
+        pass
