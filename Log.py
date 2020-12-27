929362513f81fb0e4529f69e3f1af6ae8882fe95
Omri Zaiman <omri162004@gmail.com>
2020-12-27 18:06:13 +0200

    added full Log Class

--- Log.py
+++ Log.py
@@ -3,9 +3,21 @@ import Entity
 class Log():
     
     
-    def __init__(self, log_path):
+    def __init__(self, log_path, overwrite = True):
         self.file_path = log_path
 
+        try:
+            if overwrite:
+                self.log_file = open(log_path, 'w')
+            else:
+                self.log_file = open(log_path, 'a')
+        except:
+            raise Exception("couldn't create log")
+
+    def __del__(self):
+        self.log_file.close()
+
+
     def add_record(self, entity, level):
-        #TODO: fill this function
-        pass
+        st = "Entity in IP address " + entity.get_ip_add() + ", is blocked in blocking level " + str(level) + "!\n"
+        self.log_file.write(st)
