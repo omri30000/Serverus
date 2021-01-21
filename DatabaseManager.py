7ad2174bb31cc2afae1e0ba00901f7d78c52de3c
Omri Zaiman <omri162004@gmail.com>
2021-01-21 19:10:55 +0200

    added insert product option to db manager

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,13 +1,25 @@
 import sqlite3
 import Event
+import datetime
 
 class DatabaseManager:
     def __init__(self, db_file_name):
         self.db = sqlite3.connect(db_file_name)
+        self.db_cursor = self.db.cursor()
 
-    def insert_event(self, event,computer_id):
-        '''
-        INSERT INTO Events (productId, attackerIp, blockLevel)
-        VALUES (1,"5.5.5.5", 3)
-        '''
-        pass
+
+    def insert_product(self)
+        sql_statement = '''INSERT INTO Products (joinDate)
+        VALUES (''' + str(datetime.datetime.now()) + ")"
+        self.db_cursor.execute(sql_statement)
+
+        sql_statement = "SELECT * FROM Products WHERE joinDate=" + str(datetime.datetime.now()) 
+        self.db_cursor.execute(sql_statement)
+
+
+
+    def insert_event(self, event, product_id):
+        sql_statement = '''INSERT INTO Events (productId, attackerIp, blockLevel)
+        VALUES (1,"5.5.5.5", 3)'''
+        self.db_cursor.execute(sql_statement)
+
