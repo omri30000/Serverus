89770ae68bd7c40136bce18f14bc20a4ccb13d30
Omri Zaiman <omri162004@gmail.com>
2021-01-23 17:46:19 +0200

    added get_dangerous_events method

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -19,6 +19,9 @@ class DatabaseManager:
         self.db.commit()
         self.db.close()
 
+
+
+    # -----------to be moved to the http server---------------------
     def insert_user(self, username, password, product_id):
         sql_statement = "INSERT INTO Users (username, password, computerId) VALUES (\'" + username + "\', \'" + password + "\', " + str(product_id) + ")"
         self.db_cursor.execute(sql_statement)
@@ -33,12 +36,29 @@ class DatabaseManager:
 
         for row in rows:
             print(row)
-
+    # --------------------------------------------------------------
 
     def insert_event(self, event, product_id):
         sql_statement = '''INSERT INTO Events (productId, attackerIp, blockLevel)
         VALUES (''' + str(product_id) + ", \'" + event.get_ip_add() + "\', " + event.get_level() + ")"
         self.db_cursor.execute(sql_statement)
+
+
+    def get_dangerous_events(self, time, product_id):
+        """
+        The method will find all the events of level 4 that isn't related to the given user and appeared after a specific time
+        :param self: the instance of manager
+        :type self: DatabaseManager
+        :param time: the minimum approved date of an event
+        :type time: Datetime
+        :param product_id: the id of the product that needs the data
+        :type product_id: int
+	    :return: no return value
+	    :rtype: None
+	    """
+        sql_statement = "SELECT * FROM Products WHERE productId != " + str(product_id) + " AND blockLevel = 4"
+        self.db_cursor.execute(sql_statement)
+        rows = self.db_cursor.fetchall()
 
 
 
