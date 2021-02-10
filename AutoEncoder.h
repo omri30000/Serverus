2bf7ace26b7d86221628e789c26835897ad949fa
Omri Zaiman <omri162004@gmail.com>
2021-02-10 20:42:03 +0200

    changed permissions in AutoEncoder class

--- AutoEncoder.h
+++ AutoEncoder.h
@@ -6,26 +6,26 @@
 
 using std::valarray;
 
-
 class AutoEncoder
 {
-public:
-    valarray<float> _hiddenBias;
-    valarray<float> _visibleBias;
+    protected:
+        valarray<float> _hiddenBias;
+        valarray<float> _visibleBias;
 
-    float _learningRate;
-    int _inputSize;
+        float _learningRate;
+        int _inputSize;
 
-    void getHiddenLayer(valarray<float>vals ,valarray<float>& res);
-    void getVisibleLayer(valarray<float> vals,valarray<float>& res);
+        valarray<valarray<float>> _weights;
 
-    valarray<valarray<float>> _weights;
+        void getHiddenLayer(valarray<float>vals ,valarray<float>& res);
+        void getVisibleLayer(valarray<float> vals,valarray<float>& res);
+        float calcRmse(valarray<float> input,valarray<float> reconstruct);
 
-    AutoEncoder(int inputSize,float learningRate);
-    float train(valarray<float> input);
-    float feedForward(valarray<float> input);
-    float calcRmse(valarray<float> input,valarray<float> reconstruct);
+    public:
+        AutoEncoder(int inputSize,float learningRate);
 
+        float train(valarray<float> input);
+        float feedForward(valarray<float> input);
 };
 
 
