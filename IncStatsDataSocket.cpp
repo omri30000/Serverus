f50dcf5106de70898f56f2f030fce0e69366f2a4
Omri Zaiman <omri162004@gmail.com>
2021-03-04 12:02:11 +0200

    rearranged server files

--- IncStatsDataSocket.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by ofir on 31/12/2020.
-//
-
-#include "IncStatsDataSocket.h"
-
-string IncStatsDataSocket::getStreamKey(Packet pack) {
-    return pack.getSourceIP() + std::to_string(pack.getSourcePort());
-}
