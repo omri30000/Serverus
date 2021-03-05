2039219e1270a7863e06b7c5778b50a921b84a84
Omri Zaiman <omri162004@gmail.com>
2021-03-05 13:50:40 +0200

    added basic dashboard file

--- WebAppComponent/Main.py
+++ WebAppComponent/Main.py
@@ -1,19 +1,32 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 
 app = Flask(__name__)
 
+
 @app.route("/")  # if we use the domain only, we'll get here
-def home():
+def home_page():
     return render_template("index.html")
 
-@app.route("/login")
+
+@app.route("/login", methods=["POST", "GET"])
 def login_page():
+    if request.method == "POST":
+        user = request.form['username']
+
     return render_template("login.html")
+
 
 @app.route("/register")
 def register_page():
     return render_template("register.html")
+
+
+@app.route("/dashboard")
+def dashboard_page():
+    return render_template("dashboard.html")
 
 
 if __name__ == '__main__':
