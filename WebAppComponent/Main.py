5734680e50a79a5dd584319f504c7a1ee3e7ea14
Omri Zaiman <omri162004@gmail.com>
2021-03-05 11:11:29 +0200

    added basic index and login pages

--- WebAppComponent/Main.py
+++ WebAppComponent/Main.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")  # if we use the domain only, we'll get here
 def home():
-    return "home page"
+    return render_template("index.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/register")
+def register_page():
+    return "register page"
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
